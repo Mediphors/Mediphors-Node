efcc5209407741cad5a6d17a79347a9f301c2fe7
Ryan Kennedy <rskennedy99@gmail.com>
2021-07-15 13:16:09 -0700

    Update Register.js

--- src/Account/Register.js
+++ src/Account/Register.js
@@ -14,36 +14,42 @@ router.use(myLogger);
 
 var MongoClient = require('mongodb').MongoClient
 var url = process.env.MONGO_URI
+var key = process.env.REGISTRATION_KEY 
 
 router.post('/', function(req, res) {
-    res.cookie('cookie', 'value', { sameSite: 'none', secure: true });
-    var username = String(req.body.username)
-    var password = String(req.body.password)
-    inserted = false
-    MongoClient.connect(url, function(err,db) {
-        if (err) throw err
-        var dbo = db.db("Mediphors")
-        const saltRounds = 10;
-        bcrypt.hash(password, saltRounds, function(err, hash) {
+    if (req.body.key == key) {
+        res.cookie('cookie', 'value', { sameSite: 'none', secure: true });
+        var username = String(req.body.username)
+        var password = String(req.body.password)
+        inserted = false
+        MongoClient.connect(url, function(err,db) {
             if (err) throw err
-            var loginInfo = {username: username, password: hash}
-            var query = {username: username}
-            dbo.collection("Login").find(query).toArray(function(err, results) {
+            var dbo = db.db("Mediphors")
+            const saltRounds = 10;
+            bcrypt.hash(password, saltRounds, function(err, hash) {
                 if (err) throw err
-                if (results.length == 0) {
-                    dbo.collection("Login").insertOne(loginInfo, function(err, res) {
-                        if (err) throw err
-                        console.log("Inserted user: ", res)
-                        db.close
-                        inserted = true
-                    })
-                } else {
-                    console.log("User with same username")
-                }
+                var loginInfo = {username: username, password: hash}
+                var query = {username: username}
+                dbo.collection("Login").find(query).toArray(function(err, results) {
+                    if (err) throw err
+                    if (results.length == 0) {
+                        dbo.collection("Login").insertOne(loginInfo, function(err, res) {
+                            if (err) throw err
+                            console.log("Inserted user: ", res)
+                            db.close
+                            inserted = true
+                        })
+                    } else {
+                        console.log("User with same username")
+                    }
+                })
             })
         })
-    })
-    if (inserted) ; else res.send("Error")
+        if (inserted) ; else res.send("Error")
+    } else {
+        console.log("Wrong Key")
+        res.send("400")
+    }
 })
 
 module.exports = router;
